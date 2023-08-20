8da59f97612af53e15d13bbebe1360699b99fc94
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-08-20 18:47:51 +0200

    Change about screen for GoobySoft

--- STM32 PLC for GoobySoft/Core/Src/STM32 PLC/Touch Screen/Frames/About_stm32_plc.c
+++ STM32 PLC for GoobySoft/Core/Src/STM32 PLC/Touch Screen/Frames/About_stm32_plc.c
@@ -26,16 +26,16 @@ void STM32_PLC_LCD_Show_About_STM32_PLC_Frame(uint8_t *frame_id){
 	ILI9341_print_text("Digital inputs, Differential ADC 16-bit", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("Input capture and Encoder. See the document", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("of this PCB board in the C++ software:", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
-	ILI9341_print_text("OpenSourceLogger", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
+	ILI9341_print_text("GoobySoft", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("Whit that software, you can store data", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("directly to a SQL database. Also control", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("the outputs of this PCB board. This STM32", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("processor STM32F373VBT is written with C", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("programming language. Visit:", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("https://github.com/danielmartensson/", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
-	ILI9341_print_text("and look for the OpenSourceLogger", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
+	ILI9341_print_text("and look for the GoobySoft", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 	ILI9341_print_text("repository there. Run the software.", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
-	ILI9341_print_text("Created: 2022-05-25", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
+	ILI9341_print_text("Created: 2023-08-20", 55, y += 10, COLOR_YELLOW, COLOR_NAVY, 1);
 
 
 	/* Button */
